077cb55f61fa5bead5806722932bfe8ea76e983e
hhu <hhu@thoughtworks.com>
2019-03-22 23:13:21 +0800

    init

---
+++ src/main/java/study/huhao/demo/graphqljavademo/GraphqlJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package study.huhao.demo.graphqljavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlJavaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/huhao/demo/graphqljavademo/GraphqlJavaDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package study.huhao.demo.graphqljavademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GraphqlJavaDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
